338d8a61269e0f57bfdff9664a37fcdb57753ac0
norainnoi <67267366+norainnoi@users.noreply.github.com>
2022-02-20 16:18:58 +0530

    test

--- Main_test.go
+++ Main_test.go
@@ -1,41 +1,37 @@
 // main_test.go
 package main_test
- 
+
 import (
-     "os"
-     "testing"
-     "log"
-     "net/http"
-     "net/http/httptest"
-     "strconv"
-     "encoding/json"
-     "bytes"
-		 "github.com/NaharEmet/Kajimba_server_v1"
- )
+	"log"
+	"os"
+	"testing"
+
+	main "github.com/NaharEmet/Kajimba_server_v1"
+)
 
 var a main.App
 
 func TestMain(m *testing.M) {
-    a.Initialize(
-        os.Getenv("APP_DB_USERNAME"),
-        os.Getenv("APP_DB_PASSWORD"),
-        os.Getenv("APP_DB_NAME"))
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
-    ensureTableExists()
-    code := m.Run()
-    clearTable()
-    os.Exit(code)
+	ensureTableExists()
+	code := m.Run()
+	clearTable()
+	os.Exit(code)
 }
 
 func ensureTableExists() {
-    if _, err := a.DB.Exec(tableCreationQuery); err != nil {
-        log.Fatal(err)
-    }
+	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func clearTable() {
-    a.DB.Exec("DELETE FROM products")
-    a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+	a.DB.Exec("DELETE FROM products")
+	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
 }
 
 const tableCreationQuery = `CREATE TABLE IF NOT EXISTS products
